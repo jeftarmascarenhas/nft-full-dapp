aecf1554c5db33f3ecbafd950000587ba83c8efe
Jeftar Mascarenhas <jeftarmascarenhas@hotmail.com>
2023-08-19 20:57:25 -0300

    feat: removing wagmi provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,29 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { configureChains, createConfig, WagmiConfig } from "wagmi";
-import { goerli } from "wagmi/chains";
-import { publicProvider } from "wagmi/providers/public";
-
 import "./index.css";
-import { MetaMaskConnector } from "wagmi/connectors/metaMask";
-
-const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [goerli],
-  [publicProvider()]
-);
-
-const config = createConfig({
-  autoConnect: true,
-  publicClient,
-  webSocketPublicClient,
-  connectors: [new MetaMaskConnector({ chains })],
-});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <WagmiConfig config={config}>
-      <App />
-    </WagmiConfig>
+    <App />
   </React.StrictMode>
 );
