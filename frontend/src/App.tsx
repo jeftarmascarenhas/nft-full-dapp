d386023a3c65516d05ae1ab82dd87cefb60962bc
Jeftar Mascarenhas <jeftarmascarenhas@hotmail.com>
2023-08-19 14:38:01 -0300

    feat: create start to record youtube video to nft choose

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,62 +1,12 @@
 import { useState } from "react";
 import "./App.css";
-import { useAccount, useConnect, useContractWrite } from "wagmi";
-import NftArtifact from "./NftArtifact.json";
-import { parseEther, BaseError, ContractFunctionRevertedError } from "viem";
-
-const errorMapping: Record<string, string> = {
-  MaxSupplyExceeded: "Max supply exceeded",
-  ValueNotEnough: "Value isn't enough",
-  MaxPerWallet: "Max per wallet exceeded",
-  FailedTransfer: "Sorry, isn't possible do the transfer, try again.",
-  default: "Ops! There is some error.",
-};
 
 function App() {
   const [total, setTotal] = useState(1);
-  const {
-    writeAsync,
-    data,
-    isLoading: isMintLoading,
-  } = useContractWrite({
-    address: `0x866dc9f7F81083D21346b8B45b08a40306458178`,
-    abi: NftArtifact.abi,
-    functionName: "mint",
-  });
-
-  if (data) {
-    console.log("data =>", data);
-  }
-
-  const { isConnected } = useAccount();
-  const { connect, connectors, isLoading } = useConnect();
-  const [metamask] = connectors;
+  const isConnected = false;
 
   async function handlerSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    try {
-      if (!isConnected) {
-        connect({ connector: metamask });
-        return;
-      }
-      const price = 0.0001;
-      const value = parseEther(String(total * price));
-      await writeAsync({
-        args: [total],
-        value,
-      });
-    } catch (err) {
-      if (err instanceof BaseError) {
-        const revertError = err.walk(
-          (er) => er instanceof ContractFunctionRevertedError
-        );
-        if (revertError instanceof ContractFunctionRevertedError) {
-          const errorName = revertError.data?.errorName ?? "default";
-          console.log("revertCustomError", errorName);
-          alert(errorMapping[errorName]);
-        }
-      }
-    }
   }
 
   return (
@@ -105,11 +55,7 @@ function App() {
               placeholder="Mint your NFT"
               onChange={({ target }) => setTotal(Number(target.value))}
             />
-            <button disabled={isLoading || isMintLoading} type="submit">
-              {isLoading || isMintLoading
-                ? "Loading..."
-                : "Mint a BoredApeYach Fake"}
-            </button>
+            <button type="submit">Mint a BoredApeYach Fake</button>
             {isConnected && <p className="text-center">You are connected</p>}
           </form>
         </article>
